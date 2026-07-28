61abeda2fd4f77988129cd9fd63b9bb55fcdca44
hodinit <horatiulazar2003@gmail.com>
2026-07-28 18:23:29 +0300

    small changes

--- chapter7/20_exercise1.pl
+++ chapter7/20_exercise1.pl
@@ -3,14 +3,14 @@ use warnings;
 use diagnostics;
 
 sub average {
-    my @numbers = @_;
+    my $numbers = shift;
     my ( $total, $count ) = (0, 0);
-    foreach my $number (@numbers) {
+    foreach my $number (@$numbers) {
         $total += $number;
         $count += 1;
     }
     return $total / $count;
 }
 
-my @list = ( 1, 2, 3, 4, 39 );
-print average(@list);
+my @list = ( 1, 2, 10, 10, 10 );
+print average(\@list);
