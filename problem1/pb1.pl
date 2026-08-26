c9c9571cc4a7e0edc202b2f39bc723359ea1da1c
horatiu <horatiulazar2003@gmail.com>
2026-08-26 15:15:26 +0300

    printing month and bv

--- problem1/pb1.pl
+++ problem1/pb1.pl
@@ -2,20 +2,26 @@ use strict;
 use warnings;
 use diagnostics;
 use Data::Dumper;
-
-use Text::CSV;
-
-my $csv = Text::CSV->new( { sep_char => ',' } );
+use DateTime;
+use feature 'say';
 
 open my $file, '<', 'file.csv';
 my @lines = <$file>;
 close $file;
 
 my @new_lines;
-for my $line (@lines) {
+my @words;
+
+foreach my $line (@lines) {
     chomp $line;
-    push( @new_lines, $line );
+    my @words = split ',', $line;
+    if ( $words[0] eq 'UT' ) {
+        push @new_lines, [@words];
+    }
 }
 
-print Dumper(@new_lines);
+foreach my $element (@new_lines) {
+    print "$element->[1] $element->[2]\n";
+}
 
+# print Dumper(@new_lines);
