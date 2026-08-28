4fb7803ced6b07126b20f8e892aed4eb499af76a
horatiu <horatiulazar2003@gmail.com>
2026-08-28 15:35:35 +0300

    integrating function, yet to solve for each month

--- problem1/pb1.pl
+++ problem1/pb1.pl
@@ -10,46 +10,53 @@ my @lines = <$file>;
 close $file;
 
 my @new_lines;
+my @array_of_statistics;
 
-foreach my $line (@lines) {
-    chomp $line;
-    my @words = split ',', $line;
-    if ( $words[0] eq 'UT' ) {
-        push @new_lines, [@words];
+
+@array_of_statistics = extract_data_for_given_state('UT');
+print @array_of_statistics;
+
+
+sub extract_data_for_given_state {
+    my $state = shift;
+    foreach my $line (@lines) {
+        chomp $line;
+        my @words = split ',', $line;
+        if ( $words[0] eq $state ) {
+            push @new_lines, [@words];
+        }
     }
-}
 
-foreach my $element (@new_lines) {
-    print "$element->[1] $element->[2]\n";
-}
-print "\n";
 
-my @avg_for_month_all_states;
-foreach my $line (@lines) {
-    chomp $line;
-    my @words = split ',', $line;
-    if ( $words[1] eq '1' ) {
-        push @avg_for_month_all_states, [@words];
+    my @avg_for_month_all_states;
+    foreach my $line (@lines) {
+        chomp $line;
+        my @words = split ',', $line;
+        if ( $words[1] eq '1' ) {
+            push @avg_for_month_all_states, [@words];
+        }
     }
-}
 
-my @bv;
-my $sum = 0;
-foreach my $element (@avg_for_month_all_states) {
-    $sum += int $element->[2];
-}
-say int $sum / @avg_for_month_all_states;
-print "\n";
+    my $count = 0;
+    my $sum_top_5 = 0;
+    my $sum_all = 0;
 
-my $count = 0;
-$sum = 0;
-foreach my $element (@avg_for_month_all_states) {
-    $sum += int $element->[2];
-    $count++;
-    if ( $count > 4 ) {
-        last;
+    foreach my $element (@avg_for_month_all_states) {
+        if ( $count <5 ) {
+            $sum_top_5 += int $element->[2];
+        }
+        $count++;    
+        $sum_all += int $element->[2];
+
     }
-}
-say int $sum / 5;
+    my $var3 = int $sum_all / @avg_for_month_all_states;
+    my $var4 = int $sum_top_5/5;
 
-# print Dumper(@avg_for_month_all_states);
+
+
+    my @return_array;
+    foreach my $element (@new_lines) {
+        push @return_array, "$element->[1] $element->[2] $var3 $var4\n";
+    }
+    return @return_array;
+}
