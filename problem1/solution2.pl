f1f5bac680030dce510209f620a1b4479287ec75
horatiu <horatiulazar2003@gmail.com>
2026-09-02 18:58:50 +0300

    header extract

--- problem1/solution2.pl
+++ problem1/solution2.pl
@@ -19,72 +19,25 @@ my %month_conversion = (
     12 => 'dec',
 );
 
-my @state_extraction;
-open my $file, '<', 'file.csv';
-my @lines = <$file>;
-close $file;
+my @data   = ();
+my %states = ();
 
-my @final = extract_data_for_given_state('UT');
-print @final;
+open my $fh, '<', 'file.csv';
+my $header = <$fh>;
+my ( $state_key, $month_key, $sales_key ) = _process_header($header);
+while ( my $line = <$fh> ) {
+    push @data, _process_line($line);
+}
+close $fh;
 
-sub extract_data_for_given_state {
-    my $input_state = shift;
-    foreach my $line (@lines) {
-        chomp $line;
-        my @words = split ',', $line;
-        push @state_extraction, [@words];
-    }
+print Dumper($month_key);
 
-    my @given_state_bv;
-    foreach my $state (@state_extraction) {
-        if ( $state->[0] eq $input_state ) {
-            push @given_state_bv, [ $state->[1], $state->[2] ];
-        }
-    }
+sub _process_header {
+    my $header = shift;
+    chomp $header;
+    return split( ',', $header );
+}
 
-    my @date_array;
-    my $sum_all   = 0;
-    my $avg_all   = 0;
-    my $count     = 0;
-    my $sum_top_5 = 0;
-    my $avg_top_5 = 0;
+sub _process_line {
 
-    foreach my $date ( 1 .. 12 ) {
-        @date_array = grep { $_->[1] == $date } @state_extraction;
-        if ( @date_array != 0 ) {
-            foreach my $element (@date_array) {
-                if ( $count < 5 ) {
-                    $sum_top_5 += int $element->[2];
-                }
-                $count++;
-                $sum_all += int $element->[2];
-            }
-            $avg_all = int $sum_all / @date_array;
-            if ( $count < 5 ) {
-                $avg_top_5 = int $sum_top_5 / $count;
-            }
-            else {
-                $avg_top_5 = int $sum_top_5 / 5;
-            }
-
-            foreach my $element (@given_state_bv) {
-                if ( $date == int $element->[0] ) {
-                    push $element->@*, $avg_all, $avg_top_5;
-                }
-            }
-
-            $sum_all   = 0;
-            $count     = 0;
-            $sum_top_5 = 0;
-        }
-    }
-
-    my @return_array;
-    foreach my $element (@given_state_bv) {
-        $element->[0] = $month_conversion{ $element->[0] };
-        push @return_array,
-          "['$element->[0]' $element->[1] $element->[2] $element->[3]],\n";
-    }
-
-    return @return_array;
 }
