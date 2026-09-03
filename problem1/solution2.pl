e649a6082ee265e2f875568ca9ee940b42a70e84
horatiu <horatiulazar2003@gmail.com>
2026-09-03 17:29:49 +0300

    finished second attempt

--- problem1/solution2.pl
+++ problem1/solution2.pl
@@ -19,10 +19,10 @@ my %month_conversion = (
     12 => 'dec',
 );
 
-my @data   = ();
-my %states = ();
+my @data = ();
 
-open my $fh, '<', 'file.csv';
+open my $fh, '<', 'file.csv'
+  or die "can't open file";
 my $header = <$fh>;
 my ( $state_key, $month_key, $sales_key ) = _process_header($header);
 while ( my $line = <$fh> ) {
@@ -30,15 +30,8 @@ while ( my $line = <$fh> ) {
 }
 close $fh;
 
-# foreach my $element (@data) {
-#     say $element->%*;
-# }
-
-# print Dumper (@data);
-
 my @final = _extract_data_for_given_state('UT');
-
-# print Dumper( \@final );
+print Dumper( \@final );
 
 sub _process_header {
     my $header = shift;
@@ -60,14 +53,10 @@ sub _process_line {
 
 sub _extract_data_for_given_state {
     my $input_state = shift;
+    my %averages;
     my @return_array;
-    foreach my $entry (@data) {
-        if ( $entry->{'code'} eq $input_state ) {
-            push @return_array, [ $entry->{'month'}, $entry->{'commi'} ];
-        }
-    }
-
     my @date_array;
+
     my $sum_all   = 0;
     my $avg_all   = 0;
     my $count     = 0;
@@ -85,13 +74,40 @@ sub _extract_data_for_given_state {
             $count++;
             $sum_all += $element->{'commi'};
         }
-        print Dumper($sum_all);
-        print Dumper($sum_top_5);
-        print "\n";
+
+        if ( $count > 0 ) {
+            $avg_all = int $sum_all / @date_array;
+            if ( $count < 5 ) {
+                $avg_top_5 = int $sum_top_5 / $count;
+            }
+            else {
+                $avg_top_5 = int $sum_top_5 / 5;
+            }
+        }
+
+        $averages{$date} = {
+            all   => $avg_all,
+            top_5 => $avg_top_5,
+        };
 
         $sum_all   = 0;
         $count     = 0;
         $sum_top_5 = 0;
+        $avg_all   = 0;
+        $avg_top_5 = 0;
     }
+
+    foreach my $entry (@data) {
+        if ( $entry->{'code'} eq $input_state ) {
+            push @return_array,
+              [
+                $month_conversion{ $entry->{'month'} },
+                $entry->{'commi'},
+                $averages{ $entry->{'month'} }->{'all'},
+                $averages{ $entry->{'month'} }->{'top_5'},
+              ];
+        }
+    }
+
     return @return_array;
 }
